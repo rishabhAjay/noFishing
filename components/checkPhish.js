8301cda02ce5b8781a519fd5905fa1cbd10ffbbe
rishabhAjay <rishabhajay24@gmail.com>
2022-02-05 17:59:43 +0530

    detects shortened phishing URLs

--- components/checkPhish.js
+++ components/checkPhish.js
@@ -1,33 +1,52 @@
 import clientConfig from "../config/clientConfig.js";
+import unshort from "url-unshort";
+let uu = unshort();
 import domainList from "../domainList.js";
 const checkPhish = () => {
   clientConfig.on("messageCreate", async (msg) => {
     const domainsList = await domainList();
-
-    domainsList.map(async (ele) => {
-      if (msg.content.includes(ele)) {
+    let result = msg.content.match(
+      /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g
+    );
+    if (result) {
+      result.map(async (ele) => {
         try {
-          await msg.channel.send(`${msg.author} has posted a suspicious link.`);
-          if (msg) {
-            await msg.delete();
-          }
-        } catch (error) {
-          const guild = await clientConfig.guilds.fetch(msg.guildId);
-          const owner = guild.members.cache
-            .filter((user) => user.id === guild.ownerId)
-            .first();
-          if (error.message !== "Unknown Message") {
-            try {
-              await owner.user.send(
-                "The bot does not have permissions. Give it the Administrator permission"
-              );
-            } catch (error) {
-              clientConfig.user.setActivity(";/help");
+          let urls = await uu.expand(ele);
+          urls = urls ? urls : "nothing";
+          domainsList.map(async (ele2) => {
+            if (
+              (urls.includes(ele2) && msg.content.includes(ele)) ||
+              msg.content.includes(ele2)
+            ) {
+              try {
+                if (msg) {
+                  await msg.delete();
+                }
+                return await msg.channel.send(
+                  `${msg.author} has posted a suspicious link.`
+                );
+              } catch (error) {
+                const guild = await clientConfig.guilds.fetch(msg.guildId);
+                const owner = guild.members.cache
+                  .filter((user) => user.id === guild.ownerId)
+                  .first();
+                if (error.message !== "Unknown Message") {
+                  try {
+                    await owner.user.send(
+                      "The bot does not have permissions. Give it the Administrator permission"
+                    );
+                  } catch (error) {
+                    clientConfig.user.setActivity(";/help");
+                  }
+                }
+              }
             }
-          }
+          });
+        } catch (error) {
+          console.log(error);
         }
-      }
-    });
+      });
+    }
   });
 };
 export default checkPhish;
