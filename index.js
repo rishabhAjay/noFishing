beb09dab66a997029c80d36800f2513d207de33c
Rishabh Ajay <rishabh.a@spurtreetech.com>
2022-09-01 12:57:14 +0530

    prepped the bot for deployment

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import checkPhish from "./components/checkPhish.js";
 import createInvite from "./components/createInvite.js";
 import help from "./components/help.js";
+import http from "http";
+import cron from "node-cron";
 dotenv.config(); //initialize dotenv
 
 import clientConfig from "./config/clientConfig.js";
@@ -10,7 +12,24 @@ createInvite();
 help();
 clientConfig.on("ready", () => {
   console.log(`Logged in as ${clientConfig.user.tag}!`);
-  clientConfig.user.setActivity(";/help");
+  cron.schedule("*/6 * * * *", () => {
+    clientConfig.user.setActivity(";/help");
+  });
+});
+const port = process.env.PORT || 3000;
+
+const requestHandler = (request, response) => {
+  response.end("Hello Node.js Server!");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+
+  console.log(`server is listening on ${port}`);
 });
 
 //make sure this line is the last line
